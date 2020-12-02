e895da974f49be54fe9543dfcf00a384032c4db7
Jake Heft <jakeheft@gmail.com>
2020-12-02 16:04:24 -0700

    Create tests for dashboard features
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

---
+++ spec/features/Users/show_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'As a logged in user, on my dashboard' do
+  before :each do
+    @user = User.create(name: "Elvis", password: "test", email: 'user@email.com')
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    visit dashboard_path
+  end
+
+  it 'I see a button to discover movies' do
+    click_button 'Discover Movies'
+
+    expect(current_path).to eq(discover_path)
+  end
+
+  it 'I see a friends section where I can add a friend' do
+    brett = User.create(name: 'Brett', email: 'brett@email.com', password: 'brettspassword' )
+
+    expect(@user.friends.first).to eq(nil)
+    expect(page).to have_content('You currently have no friends')
+
+    within ".friends" do
+      fill_in :email, with: brett.email
+      click_on 'Add Friend'
+    end
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("#{brett.name} is now your friend")
+
+    within "#friend-#{brett.id}" do
+      expect(page).to have_content(brett.name)
+    end
+
+    expect(@user.friends.first).to eq(brett)
+  end
+
+  #it no email found for friend sad path
+end
