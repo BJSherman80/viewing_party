f186f655532a2110def88ea4231b8aa157b42616
Jake Heft <jakeheft@gmail.com>
2020-12-02 18:08:39 -0700

    Change validations to make rubocop happy
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  validates_presence_of :name, require: true
-  validates_presence_of :password, require: true
+  # validates_presence_of :name, require: true
+  validates :name, presence: true
+  # validates_presence_of :password, require: true
+  validates :password, presence: true
   validates :password, confirmation: { case_sensitive: true }
   validates :email, uniqueness: true, presence: true
 
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
-
 
   def duplicate_email?
     User.pluck(:email).include?(email)
