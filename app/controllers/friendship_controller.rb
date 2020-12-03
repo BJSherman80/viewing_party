3492d11fc6e9499d1c83522caa495166dbe9ce0e
Jake Heft <jakeheft@gmail.com>
2020-12-02 18:07:54 -0700

    Update friendship controller to accommodate sad path
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,9 +1,16 @@
 class FriendshipController < ApplicationController
   def create
     friend = User.find_by(email: params[:email])
-    Friendship.create(user: current_user, friend: friend)
+    friendship = Friendship.new(user: current_user, friend: friend)
+    if current_user.email == params[:email]
+      flash[:failure] = "That's your own email, silly!"
+    elsif User.find_by(email: params[:email])
+      friendship.save
     # Friendship.create(user: friend, friend: current_user)
-    flash[:success] = "#{friend.name} is now your friend"
+      flash[:success] = "#{friend.name} is now your friend"
+    else
+      flash[:failure] = "There aren't any users with that email"
+    end
     redirect_to dashboard_path
   end
 end
