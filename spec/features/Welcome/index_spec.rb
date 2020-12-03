95403b4bb44810a3805bbb25b4c4193d2a4a53aa
Dani Coleman <danicoleman00@gmail.com>
2020-12-02 20:28:10 -0700

    Change paths to path helpers
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- spec/features/Welcome/index_spec.rb
+++ spec/features/Welcome/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor" do
   it "the welcome page displays a message, description, log-in link and registration link" do
-    visit '/'
+    visit root_path
     expect(page).to have_content("Welcome to Viewing Party!")
     expect(page).to have_content("Viewing Party is an app")
     expect(page).to have_button("Log In")
@@ -10,7 +10,7 @@ RSpec.describe "As a visitor" do
   end
 
   it "can click register link" do
-    visit '/'
+    visit root_path
     expect(page).to have_button("Register New User")
     click_on "Register New User"
     expect(current_path).to eq("/registration")
