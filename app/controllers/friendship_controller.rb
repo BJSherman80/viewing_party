df25e580d16fab5aef019f2880010cd3bc4c2760
Jake Heft <jakeheft@gmail.com>
2020-12-02 20:26:05 -0700

    Remove friendship creation for both ways/people
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -6,7 +6,6 @@ class FriendshipController < ApplicationController
       flash[:failure] = "That's your own email, silly!"
     elsif User.find_by(email: params[:email])
       friendship.save
-    # Friendship.create(user: friend, friend: current_user)
       flash[:success] = "#{friend.name} is now your friend"
     else
       flash[:failure] = "There aren't any users with that email"
