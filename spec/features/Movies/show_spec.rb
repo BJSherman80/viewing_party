2ec64856f49b155bf4a8b1516b936f201d05f6e7
Brett Turing <bjsherman80@gmail.com>
2020-12-05 09:39:52 -0700

    edit movie summary in test; sufficent ammount to verify correct summary
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- spec/features/Movies/show_spec.rb
+++ spec/features/Movies/show_spec.rb
@@ -14,22 +14,15 @@ RSpec.describe 'Movies show page' do
       click_on "Find Movies"
 
       click_link "Happy Gilmore"
-
-      save_and_open_page
       expect(page).to have_button("Create Viewing Party for Movie")
       expect(page).to have_content("Happy Gilmore")
       expect(page).to have_content("Vote Average: 6.6")
       expect(page).to have_content("Runtime: 1 hr 32 min")
       expect(page).to have_content("Genre(s): Comedy")
-      expect(page).to have_content("Summary: Failed hockey player-turned-golf whiz Happy Gilmore -- whose unconventional approach and antics on the grass courts the ire of rival Shooter McGavin -- is determined to win a PGA tournament so he can save his granny’s house with the prize money. Meanwhile, an attractive tour publicist tries to soften Happy’s image.")
+      expect(page).to have_content("Summary: Failed hockey player-turned-golf whiz Happy Gilmore -- whose unconventional approach and antics on the grass courts the ire of rival Shooter McGavin -- is determined to win a PGA tournament so he can save his")
     end
   end
+  it 'can see a movies top 10 actors' do
+    
+  end
 end
-
-  # it 'can see the top ten actors on a movie show page' do
-  #
-  # end
-
-  # it 'can see the review information on a movie show page' do
-  #
-  # end
