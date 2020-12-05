d1e717a96438fb3bd8cfb19457962c6361e52383
Jake Heft <jakeheft@gmail.com>
2020-12-05 11:22:30 -0700

    Add test to click create party button
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- spec/features/Movies/show_spec.rb
+++ spec/features/Movies/show_spec.rb
@@ -24,4 +24,21 @@ RSpec.describe 'Movies show page' do
       expect(page).to have_content("Adam Sandler as Happy Gilmore")
     end
   end
+
+  it "When I click Create viewing party, I'm taken to make a new party" do
+    VCR.use_cassette("Movie_Index_Page/can_search_by_movie_title", allow_playback_repeats: true, :record => :new_episodes) do
+      user = User.create!(name: "Elvis", password: "test", email: 'user@email.com')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit discover_path
+
+      fill_in :search, with: "Happy Gilmore"
+      click_on "Find Movies"
+
+      click_link "Happy Gilmore"
+      click_button "Create Viewing Party for Movie"
+
+      expect(current_path).to eq('/parties/new')
+    end
+  end
 end
