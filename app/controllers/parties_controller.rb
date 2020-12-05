781c5043c30732c1b8d87c8bc62d6c5f75a7cf0a
Jake Heft <jakeheft@gmail.com>
2020-12-05 12:42:16 -0700

    Add create method - WIP
    Co-authored-by: Dani Coleman 
    <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman 
    <64512380+BJSherman80@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,5 +1,26 @@
 class PartiesController < ApplicationController
-  def new; end
+  def new
+    @movie = params['movie']
+  end
 
-  def create; end
+  def create
+    movie = Movie.new(movie_params)
+    party = Party.new(party_params)
+    if party.save
+      flash[:success] = 'Your viewing party has been created'
+      redirect_to dashboard_path
+    else
+      flash[:error] = 'Please try again'
+      render :new
+    end
+  end
+
+  private
+  def party_params
+    params.permit(:movie_title, :party_duration, :date, :start_time)
+  end
+
+  def movie_params
+    params.permit(:title, :runtime, :api_id)
+  end
 end
