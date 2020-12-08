37a164f1d642226b88a47d87ac3c263624dab153
Dani Coleman <danicoleman00@gmail.com>
2020-12-07 17:21:05 -0700

    Commented out the private params to manually create party and movie params and reworded the flash message
    Co-authored-by: Brett Sherman <64512380+BJSherman80@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -4,23 +4,38 @@ class PartiesController < ApplicationController
   end
 
   def create
-    movie = Movie.new(movie_params)
-    party = Party.new(party_params)
+    movie = Movie.create(
+      title: params[:movie_title],
+      runtime: params[:movie_runtime],
+      api_id: params[:movie_api_id]
+    )
+    party = Party.new(
+      date: params[:date],
+      start_time: params[:start_time],
+      party_duration: params[:party_duration],
+      movie_id: movie.id,
+      user_id: current_user.id
+    )
     if party.save
       flash[:success] = 'Your viewing party has been created'
       redirect_to dashboard_path
     else
-      flash[:error] = 'Please try again'
+      flash[:error] = 'Missing fields. Please try again.'
+      @movie = {
+        title: params[:movie_title],
+        runtime: params[:movie_runtime],
+        id: params[:movie_api_id]
+      }
       render :new
     end
   end
 
-  private
-  def party_params
-    params.permit(:movie_title, :party_duration, :date, :start_time)
-  end
+  # private
+  # def party_params
+  #   params.permit(:movie_id, :party_duration, :date, :start_time)
+  # end
 
-  def movie_params
-    params.permit(:title, :runtime, :api_id)
-  end
+  # def movie_params
+  #   params.permit(:movie_title, :movie_runtime, :movie_api_id)
+  # end
 end
