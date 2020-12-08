0db04339df1369dc98322373d0a7e54fb2911d0c
Dani Coleman <danicoleman00@gmail.com>
2020-12-07 17:20:03 -0700

    Add validations to the party model for party duration
    Co-authored-by: Brett Sherman <64512380+BJSherman80@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>

--- app/models/party.rb
+++ app/models/party.rb
@@ -5,6 +5,7 @@ class Party < ApplicationRecord
   validates :date, presence: true
   validates :start_time, presence: true
   validates :movie_id, presence: true
+  validates :party_duration, presence: true
 
   def host_status(user)
     if user.id == user_id
