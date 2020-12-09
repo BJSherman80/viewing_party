8afdf2d11584ea3f4b0e92b2e40f03dadb55c3a9
Jake Heft <jakeheft@gmail.com>
2020-12-08 21:21:05 -0700

    Streamline controllers

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,45 +13,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
-      f.params['api_key'] = ENV['MOVIE_API_KEY']
-    end
-
-    @movies = []
-    page_num = 0
-    2.times do
-      page_num += 1
-
-      response = conn.get('/3/movie/top_rated') do |req|
-        req.params[:page] = page_num
-      end
-
-      movie_data = JSON.parse(response.body, symbolize_names: true)
-      @movies << movie_data[:results]
-    end
-    @movies = @movies.flatten
+    @movies = MovieFacade.fetch_top_40_movie_data
   end
 
   def search
-    search_word = params[:search]
-
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
-      f.params['api_key'] = ENV['MOVIE_API_KEY']
-    end
-
-    @search_results = []
-    page_num = 0
-    2.times do
-      page_num += 1
-
-      response = conn.get('/3/search/movie') do |req|
-        req.params[:query] = search_word
-        req.params[:page] = page_num
-      end
-
-      movie_data = JSON.parse(response.body, symbolize_names: true)
-      @search_results << movie_data[:results]
-    end
-    @search_results = @search_results.flatten
+    @search_results = SearchFacade.fetch_movie_data(params[:search])
   end
 end
