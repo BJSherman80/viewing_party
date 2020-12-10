aead1db60e44c167dad5da0cfb71b3c610753b72
Jake Heft <jakeheft@gmail.com>
2020-12-09 19:10:06 -0700

    Refactor controllers to streamline actions
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,22 +1,17 @@
 class MoviesController < ApplicationController
   def show
-    conn = Faraday.new(url: 'https://api.themoviedb.org') do |f|
-      f.params['api_key'] = ENV['MOVIE_API_KEY']
-    end
-    movie_id = params[:id]
-    movie_stats = conn.get("/3/movie/#{movie_id}")
-    @movie_stats = JSON.parse(movie_stats.body, symbolize_names: true)
-    movie_reviews = conn.get("/3/movie/#{movie_id}/reviews")
-    @movie_reviews = JSON.parse(movie_reviews.body, symbolize_names: true)
-    movie_cast = conn.get("/3/movie/#{movie_id}/credits")
-    @movie_cast = JSON.parse(movie_cast.body, symbolize_names: true)
+    @movie = MovieFacade.movie_details(params[:id])
+    @reviews = MovieFacade.fetch_movie_reviews(params[:id])
+    @cast = MovieFacade.fetch_movie_cast(params[:id])
   end
 
   def index
-    @movies = MovieFacade.fetch_top_40_movie_data
+    # @movies = MovieFacade.fetch_top_40_movies
+    # @search_results = MovieFacade.fetch_movie_search_results(params[:search]) if params[:search]
+    @movies = MovieFacade.top_rated_or_search(params[:search])
   end
-
-  def search
-    @search_results = SearchFacade.fetch_movie_data(params[:search])
-  end
+  #
+  # def search
+  #   require "pry"; binding.pry
+  # end
 end
