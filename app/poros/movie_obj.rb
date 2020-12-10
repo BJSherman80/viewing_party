158d26cc75e137920eb8db5a40d427c4b48a9292
Jake Heft <jakeheft@gmail.com>
2020-12-09 21:56:44 -0700

    Add release date to movie PORO
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>

--- app/poros/movie_obj.rb
+++ app/poros/movie_obj.rb
@@ -1,5 +1,5 @@
 class MovieObj
-  attr_reader :title, :runtime, :vote_average, :api_id, :genres, :summary
+  attr_reader :title, :runtime, :vote_average, :api_id, :genres, :summary, :release_date
 
   def initialize(attr)
     @title = attr[:title]
@@ -8,5 +8,6 @@ class MovieObj
     @api_id = attr[:id]
     @genres = attr[:genres]
     @summary = attr[:overview]
+    @release_date = attr[:release_date]
   end
 end
