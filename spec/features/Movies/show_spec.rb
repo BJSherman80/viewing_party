3cddf354a1dae42980a10d3510f5faefdb73ad07
Jake Heft <jakeheft@gmail.com>
2020-12-09 18:45:19 -0700

    Add test for movie views
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman <64512380+BJSherman80@users.noreply.github.com>

--- spec/features/Movies/show_spec.rb
+++ spec/features/Movies/show_spec.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 RSpec.describe 'Movies show page' do
   it 'can see movie attributes' do
-    VCR.use_cassette("Movie_Index_Page/can_search_by_movie_title", allow_playback_repeats: true, :record => :new_episodes) do
+    # VCR.use_cassette("Movie_Index_Page/can_search_by_movie_title", allow_playback_repeats: true, :record => :new_episodes) do
       user = User.create!(name: "Elvis", password: "test", email: 'user@email.com')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -22,11 +22,11 @@ RSpec.describe 'Movies show page' do
       expect(page).to have_content("Summary: Failed hockey player-turned-golf whiz Happy Gilmore -- whose unconventional approach and antics on the grass courts the ire of rival Shooter McGavin -- is determined to win a PGA tournament so he can save his")
       expect(page).to have_content("1 Review(s)")
       expect(page).to have_content("Adam Sandler as Happy Gilmore")
-    end
+    # end
   end
 
   it "When I click Create viewing party, I'm taken to make a new party" do
-    VCR.use_cassette("Movie_Index_Page/can_search_by_movie_title", allow_playback_repeats: true, :record => :new_episodes) do
+    # VCR.use_cassette("Movie_Index_Page/can_search_by_movie_title", allow_playback_repeats: true, :record => :new_episodes) do
       user = User.create!(name: "Elvis", password: "test", email: 'user@email.com')
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
@@ -39,6 +39,6 @@ RSpec.describe 'Movies show page' do
       click_button "Create Viewing Party for Movie"
 
       expect(current_path).to eq('/parties/new')
-    end
+    # end
   end
 end
