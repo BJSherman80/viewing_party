5a8ed51450eddbbb093bb498b6cb3823739c153a
Jake Heft <jakeheft@gmail.com>
2020-12-09 19:38:02 -0700

    Remove commented code
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,12 +6,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @movies = MovieFacade.fetch_top_40_movies
-    # @search_results = MovieFacade.fetch_movie_search_results(params[:search]) if params[:search]
     @movies = MovieFacade.top_rated_or_search(params[:search])
   end
-  #
-  # def search
-  #   require "pry"; binding.pry
-  # end
 end
